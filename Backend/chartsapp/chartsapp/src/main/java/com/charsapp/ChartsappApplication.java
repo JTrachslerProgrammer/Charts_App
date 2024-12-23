6331399b4754d9ce391de0c3711293d5b09f7582
JtrachslerProgrammer <j.trachsler@outlook.com>
2024-12-23 19:59:41 +0100

    Backend

---
+++ Backend/chartsapp/chartsapp/src/main/java/com/charsapp/ChartsappApplication.java
@@ -0,0 +1,13 @@
+package com.charsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChartsappApplication.class, args);
+	}
+
+}
